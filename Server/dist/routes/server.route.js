6d945a3a86fc4f054efe727994846c4771331fed
Student apnaCollege <priyanshukumarbgs066@gmail.com>
2025-09-15 11:48:58 +0530

    Deleating gitIgnore file

--- Server/dist/routes/server.route.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-const router = Router();
-router.get("/", (req, res) => {
-    res.send("Server route working ✅");
-});
-export default router;
-//# sourceMappingURL=server.route.js.map
--- Server/dist/service/ai.service.d.ts
+++
@@ -1,3 +0,0 @@
-declare function generatePrompt(prompt: string): Promise<string>;
-export default generatePrompt;
-//# sourceMappingURL=ai.service.d.ts.map
